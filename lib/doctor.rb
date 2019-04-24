f0f53e67b788545e021b62591104e95ce53f6195
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-24 02:10:38 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -18,7 +18,7 @@ class Doctor
   # end
 
   def new_appointment(date, patient)
-     Appointment.new(patient, self, date)
+     appointment = Appointment.new(patient, self, date)
      @appointments << appointment
      appointment
   end
