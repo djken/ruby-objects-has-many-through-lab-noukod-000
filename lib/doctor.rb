3d1012c65e97badb1f2edc8ab9bfefab072b44e8
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-24 02:15:19 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -23,12 +23,6 @@ class Doctor
      appointment
   end
 
-  def appointments
-    @@appointments.select do |appointment|
-    appointment.doctor == self
-  end
-end
-
   def patients
     @appointments.collect do |appointment|
       appointment.patient
