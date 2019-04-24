28b1e1896c576a89c9cfb30e2b5eec77bec80ea7
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-24 01:51:17 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -9,24 +9,24 @@ class Doctor
 #     @appointments=[]
 #     @@all << self
 #   end
-# 
+#
 #   def new_appointment(date, patient)
 #      appointment = Appointment.new(patient,self,date)
 #      @appointments << appointment
 #      appointment
 #   end
-# 
+#
 #   def self.all
 #     @@all
 #   end
-# 
+#
 #   def patients
 #     @appointments.collect do |appointment|
 #       appointment.patient
 #     end
 #   end
 # end
-  
+
   def initialize(name)
     @name = name
     @appointments = []
