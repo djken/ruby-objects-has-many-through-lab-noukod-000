e01af8b529940f0fa8585fe7804ace553dcd6d55
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-24 01:10:56 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,29 +1,29 @@
 # class Doctor
 #   attr_accessor :name
-#   
+#
 #   @@all = []
-#   
+#
 #   def initialize(name)
 #     @name = name
 #     self.class.all << self
 #   end
-#   
+#
 #   def self.all
 #     @@all
 #   end
-# 
+#
 #   def new_appointment(patient, date)
 #     Appointment.new(date, patient, self)
 #   end
-# 
+#
 #   def appointments
-#     Appointment.all.select do |appointment| 
+#     Appointment.all.select do |appointment|
 #       appointment.doctor == self
 #     end
 #   end
-# 
+#
 #   def patients
-#     appointments.map do |appointment| 
+#     appointments.map do |appointment|
 #       appointment.patient
 #     end
 #   end
