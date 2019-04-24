08ec19f33010830ce937a9a3d2e50ec746d08f6a
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-24 01:51:33 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -4,43 +4,25 @@ class Doctor
 
   attr_accessor :name, :appointments
 
-#   def initialize(name)
-#     @name=name
-#     @appointments=[]
-#     @@all << self
-#   end
-#
-#   def new_appointment(date, patient)
-#      appointment = Appointment.new(patient,self,date)
-#      @appointments << appointment
-#      appointment
-#   end
-#
-#   def self.all
-#     @@all
-#   end
-#
-#   def patients
-#     @appointments.collect do |appointment|
-#       appointment.patient
-#     end
-#   end
-# end
-
   def initialize(name)
-    @name = name
-    @appointments = []
+    @name=name
+    @appointments=[]
+    @@all << self
   end
 
-  def add_appointment(appointment)
-    @appointments << appointment
+  def new_appointment(date, patient)
+     appointment = Appointment.new(patient,self,date)
+     @appointments << appointment
+     appointment
   end
 
-  def appointments
-    @appointments
+  def self.all
+    @@all
   end
 
   def patients
-    self.appointments.collect {|appointment| appointment.patient}
+    @appointments.collect do |appointment|
+      appointment.patient
+    end
   end
 end
